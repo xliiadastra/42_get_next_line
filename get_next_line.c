b9309c2e63e80864d34550120258c89974544654
root <root@LAPTOP-TSQJQQ7C.localdomain>
2021-12-21 00:39:49 +0900

    wow

---
+++ get_next_line.c
@@ -0,0 +1,16 @@
+#include "get_next_line.h"
+
+void	backup_line(char *read_str)
+{
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*milk;
+	char		*butter[BUFFER_SIZE];
+
+	if (fd < 0)
+		return (NULL);
+	read(fd, butter, BUFFER_SIZE);
+	return (milk);
+}
---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct 
+
+char	*get_next_line(int fd);
+
+#endif
