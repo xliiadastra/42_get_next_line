b08ef7181032eb23410464ded24e5f732988d773
Yihyun Choi <yichoi@c3r6s5.42seoul.kr>
2022-05-22 20:42:44 +0900

    fini

--- get_next_line.h
+++ get_next_line.h
@@ -1,20 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yichoi <yichoi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/25 17:51:57 by yichoi            #+#    #+#             */
+/*   Updated: 2022/04/09 20:44:21 by yichoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
 
 # include <unistd.h>
 # include <stdlib.h>
 
-typedef struct s_butter
-{
-	unsigned char	*butter[BUFFER_SIZE];
-	int				fd;
-	struct s_butter *next;
-}	t_buffer;
-
 char	*get_next_line(int fd);
+char	*saving_line(int fd, char *storage);
+char	*parsing_read(char *storage);
+char	*new_storage(char *storage);
+int		ft_strlen(char *str);
+char	*gnl_strchr(char *str, int c);
+char	*gnl_str_catcpy(char *join, char *dst, char *src);
+char	*gnl_strdup(char *str);
+char	*gnl_strjoin(char *dst, char *src);
 
 #endif
